b92325224b5f4d6fcda91bb563ddff53bf94d0ad
Javed Muckett <javedmuckett@gmail.com>
2024-08-18 20:22:55 -0400

    dev dependencies added

--- router/authRoute.js
+++ router/authRoute.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const {User, Blog} = require('../models/data_model');
+
 
 router.get('/profile', async (req,res) => {
     res.render('profile', {title: 'Profile Page'});
 });
+
+
+router.post('/create-blog/post', async (req,res,next) => {
+    const user = await User.find({email: req.user.email});
+    const {title, content} = req.body;
+    const blog = new Blog({email: user[0], title: title, content: content});
+    await blog.save();
+    res.redirect('/');
+
+})
 
 module.exports = router;
